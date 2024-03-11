0b28e38587e2766187cb525db62ffec05f1b726a
Krishan Nissanka <krishan.nishshanka@protonmail.com>
2024-03-11 09:42:23 +0530

    first commit

---
+++ themes/tokyo-night.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#1a1b26";
+static const char col_gray2[]       = "#1a1b26";
+static const char col_gray3[]       = "#c0caf5";
+static const char col_gray4[]       = "#1a1b26";
+static const char col_cyan[]        = "#7aa2f7";
+
